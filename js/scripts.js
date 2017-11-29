1860fffbd3db2fc1d14b9cafad7bd64913a88a22
James Donlan and Kate Trahan <kate@>
2017-11-29 15:04:57 -0800

    now takes name, address and order and returns them

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
-    $("#return").show();
+    $("#response").show();
     event.preventDefault();
     var namePatient = $("input#nameEntry").val();
+    var addressData = $("input#addressEntry").val();
+    var item = $("#item").val();
     $(".name").text(namePatient);
+    $(".address").text(addressData);
+    $(".purchase").text(item);
   });
 });
